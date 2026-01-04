381edf064b087c8fdb28edbc6235b6b0d98b45cb
Devin Green <design@devingreen.me>
2026-01-04 14:13:41 -0600

    Simplify highlights to match actual parser nodes

--- languages/october/highlights.scm
+++ languages/october/highlights.scm
@@ -1,12 +1,10 @@
-; Highlighting for October CMS templates
+; October CMS Template Highlighting
 
 ; ===== Configuration Section (INI) =====
 (ini_section_header) @type
 (ini_key) @property
 (ini_value) @string
 (comment_line) @comment
-
-; Section separators
 (section_separator) @punctuation.special
 
 ; ===== PHP Section =====
@@ -14,13 +12,10 @@
 
 ; ===== Twig Section =====
 
-; HTML Content
-(content) @none
-
-; Comments
+; Twig Comments
 (comment) @comment
 
-; Delimiters
+; Twig Delimiters - these are literal tokens
 "{%" @punctuation.bracket
 "%}" @punctuation.bracket
 "{{" @punctuation.bracket
@@ -28,100 +23,60 @@
 "{#" @punctuation.bracket
 "#}" @punctuation.bracket
 
-; Statement types
-(set_statement) @keyword
-(if_statement) @keyword
-(for_statement) @keyword
-(block_statement) @keyword
-(macro_statement) @keyword
-(import_statement) @keyword
-(from_statement) @keyword
-(filter_statement) @keyword
-(auto_escape_statement) @keyword
-(do_statement) @keyword
-(flush_statement) @keyword
-(verbatim_statement) @keyword
-(apply_statement) @keyword
-(embed_statement) @keyword
-(use_statement) @keyword
-
-; October CMS specific statements
-(page_statement) @keyword.special
-(partial_statement) @keyword.special
-(component_statement) @keyword.special
-(content_statement) @keyword.special
-(placeholder_statement) @keyword.special
-(flash_statement) @keyword.special
-
-; Generic statements
-(generic_statement) @keyword
-
-; Operators (binary and unary expressions)
-(binary_expression) @operator
-(unary_expression) @operator
-
-; Punctuation
-[":" "," "." "|"] @punctuation.delimiter
-["(" ")" "[" "]" "{" "}"] @punctuation.bracket
-["?" "="] @operator
+; Twig Directives
+(statement_directive) @keyword
+(output_directive) @variable
 
 ; Literals
 (string) @string
 (number) @number
-(boolean) @boolean
+(boolean) @constant.builtin
 (null) @constant.builtin
 
-; String interpolation
-(interpolation
-  "#{" @punctuation.special
-  "}" @punctuation.special)
-
-; Identifiers and Variables
+; Identifiers
 (identifier) @variable
 
 ; Function calls
 (function_call
   function: (identifier) @function)
 
-; Filter expressions
-(filter_expression
-  filter: (identifier) @function.builtin)
-
-; Test expressions
-(test_expression
-  test: (identifier) @function.builtin)
-
-; Member access
+; Member expressions
 (member_expression
   property: (identifier) @property)
 
-; Hash keys
+; Filter expressions
+(filter_expression
+  "|" @operator
+  filter: (identifier) @function.builtin)
+
+; Binary operators
+(binary_expression
+  operator: _ @operator)
+
+; Unary operators
+(unary_expression
+  operator: _ @operator)
+
+; Arrays and hashes
+(array
+  "[" @punctuation.bracket
+  "]" @punctuation.bracket)
+
+(hash
+  "{" @punctuation.bracket
+  "}" @punctuation.bracket)
+
 (hash_entry
   key: (identifier) @property)
 
-; Named arguments
-(named_argument
-  name: (identifier) @parameter)
+; Parentheses
+"(" @punctuation.bracket
+")" @punctuation.bracket
 
-; Statement fields
-(set_statement
-  variable: (identifier) @variable)
-
-(for_statement
-  variable: (identifier) @variable)
-
-(block_statement
-  name: (identifier) @label)
-
-(macro_statement
-  name: (identifier) @function)
-
-(placeholder_statement
-  name: (identifier) @label)
-
-(flash_statement
-  type: (identifier) @type)
-
-; Generic statement tags
-(generic_statement
-  tag: _ @keyword)
+; Other punctuation
+":" @punctuation.delimiter
+"," @punctuation.delimiter
+"." @punctuation.delimiter
+"|" @punctuation.delimiter
+"=" @operator
+"?" @operator
